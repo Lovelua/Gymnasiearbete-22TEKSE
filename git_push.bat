e66c33150b06e730ea6ad908b89711fc30b39c7b
Lovelua <sobacika1999@gmail.com>
2025-05-01 20:31:05 +0200

    Auto-commit: 2025-05-01 20:31:05,74

---
+++ git_push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\Chiril\OneDrive - Academedia\Documents\mina studier\Gymnasiearbete 22TEKSE & databas\Gymnasiearbete 22TEKSE"
+git add .
+git commit -m "Auto-commit: %date% %time%"
+git push origin master
