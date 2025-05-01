9285b67ee4772d153f747f8dfd18a594d6f2256a
Lovelua <sobacika1999@gmail.com>
2025-05-01 20:33:11 +0200

    Auto-commit: 2025-05-01 20:33:11,36

--- git_push.bat
+++ git_push.bat
@@ -1,5 +1,6 @@
 @echo off
-cd /d "C:\Users\Chiril\OneDrive - Academedia\Documents\mina studier\Gymnasiearbete 22TEKSE & databas\Gymnasiearbete 22TEKSE"
+cd /d "C:\Path\To\Your\Project"
 git add .
 git commit -m "Auto-commit: %date% %time%"
 git push origin master
+start "" "C:\Path\To\VSCode.exe" .  # Opens VSCode here
